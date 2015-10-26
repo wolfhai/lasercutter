517bd6ca2ba0f45999810442d21ebccfbf6f8e80
wolfhai <wolfhai@gmx.net>
2015-10-26 18:14:07 +0100

    code cleanup

--- 2313-firmware/main.c
+++ 2313-firmware/main.c
@@ -4,40 +4,43 @@
 #define WS 7
 static unsigned char step_0=0;
 static unsigned char step_1=0;
-unsigned char fs[8]={ 9, 1, 5, 4, 6, 2, 10, 8};
+unsigned char fs[8]= { 9, 1, 5, 4, 6, 2, 10, 8};
 
-ISR (INT0_vect){
-		if (PIND & (1<<PD4)){
-			if (step_0++ == WS)
-               step_0=0;
-        }
-		else
-            if (step_0-- == 0)
-                step_0=WS;
+ISR (INT0_vect)
+{
+    if (PIND & (1<<PD4))
+    {
+        if (step_0++ == WS)
+            step_0=0;
+    }
+    else if (step_0-- == 0)
+        step_0=WS;
     PORTB = fs[step_0] | (fs[step_1]<<4);
 }
 
-ISR (INT1_vect){
-		if (PIND & (1<<PD5)){
-			if (step_1++ == WS)
-               step_1=0;
-        }
-		else
-            if (step_1-- == 0)
-                step_1=WS;
+ISR (INT1_vect)
+{
+    if (PIND & (1<<PD5))
+    {
+        if (step_1++ == WS)
+            step_1=0;
+    }
+    else if (step_1-- == 0)
+        step_1=WS;
     PORTB = fs[step_0] | (fs[step_1]<<4);
 }
 
 
-int main(void){
-    DDRB = 255; 
-    DDRD = 0; 
-  	PORTB = 0; 
-	PORTD |= (1<<PD0) | (1<<PD2) | (1<<PD3) | (1<<PD4) | (1<<PD5);
+int main(void)
+{
+    DDRB = 255;
+    DDRD = 0;
+    PORTB = 0;
+    PORTD |= (1<<PD0) | (1<<PD2) | (1<<PD3) | (1<<PD4) | (1<<PD5);
     GIMSK |= (1<<INT0)| (1<<INT1);
-	MCUCR |= (1<<ISC00) | (1<<ISC01) | (1<<ISC10) | (1<<ISC11);
-    
-    sei(); 
+    MCUCR |= (1<<ISC00) | (1<<ISC01) | (1<<ISC10) | (1<<ISC11);
+
+    sei();
     while(1)
         if (PIND & (1<<PD0))
             PORTB=0;
